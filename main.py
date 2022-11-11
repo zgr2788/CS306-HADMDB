23896653f5d181d2ae9774303e0b8545e37ae1a7
zgr2788 <ozgur.beker@gmail.com>
2022-11-11 22:39:29 +0000

    Added id to statusMessage

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def main_page(request: _fastapi.Request):
 
 #*********************************************************
 
-# DOCTORS 
+# DOCTORS
 
 #*********************************************************
 
@@ -39,12 +39,12 @@ async def create_doctor(request: _fastapi.Request):
 
 @app.post("/create/doctors/")
 async def create_doctor(request : _fastapi.Request, name : str =  _fastapi.Form(), spec : str = _fastapi.Form(), db:_orm.Session = _fastapi.Depends(_services.get_db)):
-    statusMessage = "Successfully added doctor " + name + "."
     doc = _schemas._DoctorCreate(
         name = name,
-        spec = spec 
+        spec = spec
     )
-    await _services.create_doc(doc, db)
+    new_doc = await _services.create_doc(doc, db)
+    statusMessage = "Successfully added doctor " + str(new_doc.name) + " with ID: " + str(new_doc.id) + "."
     return templates.TemplateResponse('doctor_insert.html', context = {'request': request, 'statusMessage': statusMessage})
 
 # Delete a doctor
@@ -60,11 +60,11 @@ async def delete_doctor(request : _fastapi.Request, doctor_id : int = _fastapi.F
 
     if not doc_db:
         statusMessage = "Doctor with id " + str(doctor_id) + " does not exist in database!"
-    
+
     else:
         name = doc_db.name
         await _services.delete_doc(doc_id = doctor_id , db = db)
-        statusMessage = "Successfully deleted " + str(name) + " from the database."
+        statusMessage = "Successfully deleted " + str(name) + " with ID: " + str(doctor_id) + " from the database."
 
     return templates.TemplateResponse('doctor_delete.html', context = {'request': request, 'statusMessage' : statusMessage})
 
@@ -77,17 +77,17 @@ async def get_doctors_by_name(request: _fastapi.Request):
 
 @app.post("/get/doctors/", status_code = 200)
 async def get_doctors_by_name(request: _fastapi.Request, doctor_name : str = _fastapi.Form(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
-    
+
     # Wildcard search
     if doctor_name == "*":
-        statusMessage = [doctor.name for doctor in await _services.get_docs(db = db)]  
+        statusMessage = [doctor.name for doctor in await _services.get_docs(db = db)]
         return templates.TemplateResponse('doctor_selection.html', context = {'request': request, 'statusMessage': statusMessage})
 
     doc_list = [doctor.name for doctor in await _services.get_docs_by_name(doc_name = doctor_name, db = db)]
-    
+
     if doc_list:
         statusMessage = "Search Results:\n\n\n" + str(doc_list)
-    
+
     else:
         statusMessage = "No doctors found!"
 
@@ -104,4 +104,3 @@ async def get_all_doctors(db: _orm.Session = _fastapi.Depends(_services.get_db))
 #@app.get("/api/doctors/spec/{spec_name}", status_code = 200)
 #async def get_doctors_by_spec(specialization: str, db: _orm.Session = _fastapi.Depends(_services.get_db)):
 #    return await _services.get_docs_by_spec(spec_name = specialization, db = db)
-

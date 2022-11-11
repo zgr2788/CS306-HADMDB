977ebe90487790edba73724dbb1d9f94f619343a
zgr2788 <ozgur.beker@gmail.com>
2022-11-11 13:53:56 +0000

    Connected insert

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = _fastapi.FastAPI()
 
 _services.create_database()
 
-templates = _templates.Jinja2Templates(directory = "../templates")
+templates = _templates.Jinja2Templates(directory = "templates")
 
 
 #*********************************************************
@@ -26,23 +26,23 @@ templates = _templates.Jinja2Templates(directory = "../templates")
 @app.get("/create/doctors/")
 async def create_doctor(request: _fastapi.Request):
     statusMessage = "Start typing names..."
-    return templates.TemplateResponse('form.html', context = {'request': request, 'result': statusMessage})
+    return templates.TemplateResponse('doctor_insert.html', context = {'request': request, 'statusMessage': statusMessage})
 
 @app.post("/create/doctors/")
 async def create_doctor(request : _fastapi.Request, name : str =  _fastapi.Form(), spec : str = _fastapi.Form(), db:_orm.Session = _fastapi.Depends(_services.get_db)):
-    statusMessage = "Adding doctor " + name
+    statusMessage = "Successfully added doctor " + name + "."
     doc = _schemas._DoctorCreate(
         name = name,
         spec = spec 
     )
     await _services.create_doc(doc, db)
-    return templates.TemplateResponse('form.html', context = {'request': request, 'result': statusMessage})
+    return templates.TemplateResponse('doctor_insert.html', context = {'request': request, 'statusMessage': statusMessage})
 
 # Delete a doctor
 @app.get("/delete/doctors/")
 async def delete_doctor(request: _fastapi.Request):
     statusMessage = "Start typing id..."
-    return templates.TemplateResponse('form_delete.html', context = {'request': request, 'result': statusMessage})
+    return templates.TemplateResponse('doctor_delete.html', context = {'request': request, 'result': statusMessage})
 
 
 @app.post("/delete/doctors/", status_code = 204)
@@ -56,14 +56,14 @@ async def delete_doctor(request : _fastapi.Request, doctor_id : int = _fastapi.F
         await _services.delete_doc(doc_id = doctor_id , db = db)
         statusMessage = "Successfully deleted doctor with id " + str(doctor_id)
 
-    return templates.TemplateResponse('form_delete.html', context = {'request': request, 'result' : statusMessage})
+    return templates.TemplateResponse('doctor_delete.html', context = {'request': request, 'result' : statusMessage})
 
 
 # Get doctors by name
 @app.get("/get/doctors/")
-async def delete_doctor(request: _fastapi.Request):
+async def get_doctors_by_name(request: _fastapi.Request):
     statusMessage = "Start typing name..."
-    return templates.TemplateResponse('form_get.html', context = {'request': request, 'result': statusMessage})
+    return templates.TemplateResponse('doctor_select.html', context = {'request': request, 'result': statusMessage})
 
 @app.post("/get/doctors/", status_code = 200)
 async def get_doctors_by_name(request: _fastapi.Request, doctor_name : str = _fastapi.Form(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
@@ -75,7 +75,7 @@ async def get_doctors_by_name(request: _fastapi.Request, doctor_name : str = _fa
     else:
         statusMessage = "No doctors found!"
 
-    return templates.TemplateResponse('form_get.html', context = {'request': request, 'result': statusMessage})
+    return templates.TemplateResponse('doctor_select.html', context = {'request': request, 'result': statusMessage})
 
 # Get all doctors - debug
 @app.get("/api/get/doctors", status_code = 200)

690a1f05e821ab55d8a2ce910b65d6f5ddb4507d
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2022-12-13 16:32:43 +0000

    Implement nurse admin panel

--- main.py
+++ main.py
@@ -58,7 +58,6 @@ async def delete_doctor(request: _fastapi.Request):
     statusMessage = ""
     return templates.TemplateResponse('doctor_delete.html', context = {'request': request, 'statusMessage': statusMessage})
 
-
 @app.post("/delete/doctors/", status_code = 204)
 async def delete_doctor(request : _fastapi.Request, doctor_id : int = _fastapi.Form(), db:_orm.Session = _fastapi.Depends(_services.get_db)):
     doc_db = await _services.get_doc_by_id(doctor_id, db)
@@ -72,7 +71,6 @@ async def delete_doctor(request : _fastapi.Request, doctor_id : int = _fastapi.F
         statusMessage = "Successfully deleted " + str(name) + " with ID: " + str(doctor_id) + " from the database."
 
     return templates.TemplateResponse('doctor_delete.html', context = {'request': request, 'statusMessage' : statusMessage})
-
 
 # Get doctors by name
 @app.get("/get/doctors/")
@@ -101,3 +99,77 @@ async def get_doctors_by_name(request: _fastapi.Request, doctor_name : str = _fa
 async def get_all_doctors(request: _fastapi.Request, db: _orm.Session = _fastapi.Depends(_services.get_db)):
     docs_list = await _services.get_docs(db = db)
     return templates.TemplateResponse('doctor_display.html', context = {'request' : request, 'docs_list' : docs_list})
+
+
+#*********************************************************
+
+# NURSES
+
+#*********************************************************
+# Get nurse home
+@app.get("/home/nurses/")
+async def home_nurse(request: _fastapi.Request):
+    return templates.TemplateResponse('nurse_home.html', context = {'request' : request})
+
+# Create a nurse
+@app.get("/create/nurses/")
+async def create_nurse(request: _fastapi.Request):
+    statusMessage = ""
+    return templates.TemplateResponse('nurse_insert.html', context = {'request': request, 'statusMessage': statusMessage})
+
+@app.post("/create/nurses/")
+async def create_nurse(request : _fastapi.Request, name : str =  _fastapi.Form(), db:_orm.Session = _fastapi.Depends(_services.get_db)):
+    nur = _schemas._NurseCreate(
+        name = name
+    )
+    new_nur = await _services.create_nur(nur, db)
+    statusMessage = "Successfully added nurse " + str(new_nur.name) + " with ID: " + str(new_nur.id) + "."
+    return templates.TemplateResponse('nurse_insert.html', context = {'request': request, 'statusMessage': statusMessage})
+
+# Delete a doctor
+@app.get("/delete/nurses/")
+async def delete_nurse(request: _fastapi.Request):
+    statusMessage = ""
+    return templates.TemplateResponse('nurse_delete.html', context = {'request': request, 'statusMessage': statusMessage})
+
+@app.post("/delete/nurses/", status_code = 204)
+async def delete_nurse(request : _fastapi.Request, nurse_id : int = _fastapi.Form(), db:_orm.Session = _fastapi.Depends(_services.get_db)):
+    nur_db = await _services.get_nur_by_id(nurse_id, db)
+
+    if not nur_db:
+        statusMessage = "Nurse with id " + str(nurse_id) + " does not exist in database!"
+
+    else:
+        name = nur_db.name
+        await _services.delete_nur(nur_id = nurse_id , db = db)
+        statusMessage = "Successfully deleted " + str(name) + " with ID: " + str(nurse_id) + " from the database."
+
+    return templates.TemplateResponse('nurse_delete.html', context = {'request': request, 'statusMessage' : statusMessage})
+
+# Get doctors by name
+@app.get("/get/nurses/")
+async def get_nurses_by_name(request: _fastapi.Request):
+    statusMessage = ""
+    return templates.TemplateResponse('nurse_selection.html', context = {'request': request, 'statusMessage': statusMessage})
+
+@app.post("/get/nurses/", status_code = 200)
+async def get_nurses_by_name(request: _fastapi.Request, nurse_name : str = _fastapi.Form(), db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    statusMessage = ""
+
+    # Wildcard search
+    if nurse_name == "*":
+        nurse_list = await _services.get_nurses(db=db)
+        return templates.TemplateResponse('nurse_selection.html', context = {'request': request, 'docs_list': nurse_list, 'statusMessage' : statusMessage})
+
+    nurse_list = await _services.get_nurs_by_name(nur_name = nurse_name, db = db)
+
+    if not nurse_list:
+        statusMessage = "No nurses found!"
+
+    return templates.TemplateResponse('nurse_selection.html', context = {'request': request, 'docs_list': nurse_list, 'statusMessage' : statusMessage})
+
+# Get all doctors - debug
+@app.get("/getall/nurses", status_code = 200)
+async def get_all_nurses(request: _fastapi.Request, db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    nurs_list = await _services.get_nurses(db = db)
+    return templates.TemplateResponse('nurse_display.html', context = {'request' : request, 'docs_list' : nurs_list})
--- services.py
+++ services.py
@@ -82,3 +82,47 @@ async def get_docs_by_spec(spec_name : str, db : _orm.Session):
 async def get_docs(db : _orm.Session):
     items = db.query(_models.Doctor).filter(_models.Doctor.spec.contains(''))
     return list(map(_schemas.Doctor.from_orm, items))
+
+#*********************************************************
+
+# NURSES
+
+#*********************************************************
+
+# Nurse query by id
+async def get_nur_by_id(id : int, db : _orm.Session):
+    return db.query(_models.Nurse).filter(_models.Nurse.id == id).first()
+
+# Create new nurse
+async def create_nur(nur : _schemas._NurseCreate, db : _orm.Session):
+        
+    # New nurse object
+    nurObj = _models.Nurse(
+        name = nur.name,
+    )
+
+    # Write to db
+    db.add(nurObj)
+    db.commit()
+    db.refresh(nurObj)
+    return nurObj
+
+# Delete nurse
+async def delete_nur(nur_id : int , db : _orm.Session):
+    nur_db =  await get_nur_by_id(nur_id, db)
+
+    if nur_db is None:
+        raise _fastapi.HTTPException(status_code=404, detail = "Nurse ID not found in database!")
+
+    db.delete(nur_db)
+    db.commit()
+
+# Get nurses by name
+async def get_nurs_by_name(nur_name : str, db : _orm.Session):
+    items = db.query(_models.Nurse).filter(_models.Nurse.name.contains(nur_name))
+    return list(map(_schemas.Nurse.from_orm, items))
+
+# Get all nurses
+async def get_nurses(db : _orm.Session):
+    items = db.query(_models.Nurse).filter(_models.Nurse.name.contains(''))
+    return list(map(_schemas.Nurse.from_orm, items))
